b49674d4cc01f55f47dfee193a9be6917a060d92
SEJAL BHARDWAJ <88772409+sejalbhardwaj@users.noreply.github.com>
2023-05-24 19:36:48 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import About from "./About";
 import Contact from "./Contact";
-import Error from "./Error";
 import Home from "./Home";
 import Service from "./Service";
 import Header from "./Components/Header";
@@ -41,7 +40,6 @@ const App = () => {
           <Route path="/service" element={<Service />} />
           <Route path="/Testimonials" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error/>} />
         </Routes>
         <Footer />
       </BrowserRouter>
